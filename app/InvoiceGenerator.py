bd28048107437077f2ce5d837afa0aaf1aeb2b39
DownsOU <ddowns@oakland.edu>
2022-04-14 04:32:04 -0400

    employee features finished

--- app/InvoiceGenerator.py
+++ app/InvoiceGenerator.py
@@ -15,7 +15,7 @@ class InvoiceGenerator:
     def createInvoice(self, custid, empid):
         self.invoiceNumber = str(uuid.uuid4().hex)[0:5]
         self.db.addInvoiceToDB(self.invoiceNumber, custid, empid)
-        self.c = canvas.Canvas("invoice" + self.invoiceNumber + ".pdf", pagesize=(200, 250), bottomup=0)
+        self.c = canvas.Canvas("invoice/invoice" + str(self.invoiceNumber) + ".pdf", pagesize=(200, 250), bottomup=0)
 
     def getInvoiceNumber(self):
         return self.invoiceNumber
@@ -29,7 +29,7 @@ class InvoiceGenerator:
         # Inverting the scale for getting mirror Image of logo
         self.c.scale(1,-1)
         # Inserting Logo into the Canvas at required position
-        self.c.drawImage("../img/ej engine.jpg",0,0,width=50,height=30)
+        self.c.drawImage("img/ej engine.jpg",0,0,width=50,height=30)
         # Title Section
         # Again Inverting Scale For strings insertion
         self.c.scale(1,-1)
@@ -74,32 +74,16 @@ class InvoiceGenerator:
         self.c.line(15,220,185,220)
         self.c.line(100,220,100,238)
         self.c.setFont("Courier-Bold", 3)
-        self.c.drawString(20,225,"(This is system generated invoice)")
+        self.c.drawString(20,229,"(This is system generated invoice)")
         self.c.setFont("Courier-Bold", 5)
         self.c.drawCentredString(130,225,"Customer Signature:")
 
-    def addInvoiceInfo(self, invnum, custid, empid):
-        invoice = self.db.getInvoice(invnum)
-        customer = self.db.getCustomer(invoice[1])
-        self.c.setFont("Times-Bold", 5)
-        #self.c.drawCentredString(90, 70, "INVOICE #: " + invoice[0])
-        #self.c.drawCentredString(90, 80, "DATE :" + str(date.today()))
-        #self.c.drawCentredString(90, 90, "CUSTOMER NAME :" + customer[0] + " " + customer[1])
-        #self.db.addInvoiceToDB(self.invoiceNumber, custid, empid)
-
     def addPartToInvoice(self, invnum, part, qty):
-        self.parts.append(part)
         self.db.addPartToInvoice(invnum, part, qty)
-        partProps = self.db.getPart(part)
-        #self.c.drawCentredString(25, self.partCoordinateY, str(partProps[0]))
-        #self.c.drawCentredString(75, self.partCoordinateY, str(partProps[2]))
-        #self.c.drawCentredString(125, self.partCoordinateY, str(partProps[3]))
-        #self.c.drawCentredString(148, self.partCoordinateY, qty)
-        #self.partCoordinateY += 5
         return
 
     def createInvoiceGraphics(self, invnum):
-        self.c = canvas.Canvas("invoice" + invnum + ".pdf", pagesize=(200, 250), bottomup=0)
+        self.c = canvas.Canvas("invoice" + str(invnum) + ".pdf", pagesize=(200, 250), bottomup=0)
         self.template()
         invoice = self.db.getInvoice(invnum)
         partProps = self.db.getPart(invoice[3])
@@ -109,38 +93,20 @@ class InvoiceGenerator:
         self.c.drawCentredString(90, 90, "CUSTOMER NAME :" + customer[0] + " " + customer[1])
 
         self.c.drawCentredString(25, self.partCoordinateY, str(partProps[0]))
-        self.c.drawCentredString(75, self.partCoordinateY, str(partProps[2]))
+        self.c.drawCentredString(75, self.partCoordinateY, str(partProps[1]))
         self.c.drawCentredString(125, self.partCoordinateY, str(partProps[3]))
-        self.c.drawCentredString(148, self.partCoordinateY, invoice[4])
+        self.c.drawCentredString(148, self.partCoordinateY, str(invoice[4]))
         self.partCoordinateY += 5
+
+        self.c.setFont("Courier-Bold", 3)
+        self.c.drawString(20,225,"Sold By: " + str(invoice[2]))
+        
         total = 0
-        for part in self.parts:
-            total = total + partProps[3]
+        total = total + partProps[3]
+        
         self.c.setFont("Times-Bold", 5)
         self.c.drawCentredString(173, 216, str(total))
         # End the Page and Start with new
         self.c.showPage()
         # Saving the PDF
         self.c.save()
-
-
-    def finishAndSaveInvoice(self):
-        total = 0
-        for part in self.parts:
-            total = total + self.db.getPart(part)[3]
-        self.c.setFont("Times-Bold", 5)
-        self.c.drawCentredString(173, 216, str(total))
-        # End the Page and Start with new
-        self.c.showPage()
-        # Saving the PDF
-        self.c.save()
-
-    def createPrintedInvoice(self):
-        self.template()
-
-"""Invoice = InvoiceGenerator()
-Invoice.createTemplate()
-Invoice.addInvoiceInfo("248-555-5555")
-Invoice.addPartToInvoice(6969)
-Invoice.addPartToInvoice(1234)
-Invoice.finishAndSaveInvoice()"""
--- app/PartPage.py
+++ app/PartPage.py
@@ -1,8 +1,10 @@
+import sys
 import tkinter as tk
 from PIL import ImageTk, Image
 from connector import DB
 import PartFinder
 import InvoiceGenerator
+import EmployeeConsole
 
 def partpageGui(partnum, invoiceNumber = None):
     db = DB()
@@ -12,7 +14,7 @@ def partpageGui(partnum, invoiceNumber = None):
 
     partProps = db.getPart(partnum)
 
-    partPicture = Image.open("../img/" + str(partProps[0])+'.jpg')
+    partPicture = Image.open("img/" + str(partProps[1])+'.jpg')
     partPicture = partPicture.resize((100, 100))
     partPicture = ImageTk.PhotoImage(partPicture)
     pictureLabel = tk.Label(partPage, image=partPicture)
@@ -27,32 +29,36 @@ def partpageGui(partnum, invoiceNumber = None):
     partPriceLabel.pack()
     qohLabel = tk.Label(descriptionFrame, text="Quantity: " + str(partProps[4]))
     qohLabel.pack()
-    availableByLabel = tk.Label(descriptionFrame, text="Available by: " + str(partProps[5]))
-    availableByLabel.pack()
     descriptionFrame.pack(side=tk.LEFT)
 
     def addToInvoice(part, qty):
         invoice = InvoiceGenerator.InvoiceGenerator()
         invoice.addPartToInvoice(invoiceNumber, part, qty)
         invoice.createInvoiceGraphics(invoiceNumber)
+        partPage.destroy()
+        window = tk.Toplevel(EmployeeConsole.EConsole())
+        window.transient(partPage)
         return
 
     def back():
         partPage.destroy()
-        window = tk.Toplevel(PartFinder.partWin())
+        window = tk.Toplevel(PartFinder.partWin(invoiceNumber))
         window.transient(partPage)
         return
 
     buttonFrame = tk.Frame(partPage)
 
-    cartButton = tk.Button(buttonFrame, text="Add To Cart", command=lambda: addToInvoice(partProps[0], 1))
+    cartButton = tk.Button(buttonFrame, text="Add To Invoice " + invoiceNumber, command=lambda: addToInvoice(partProps[0], 1))
     cartButton.pack(side=tk.LEFT)
 
     backButton = tk.Button(buttonFrame, text="Back", command=back)
     backButton.pack(side = tk.LEFT)
 
     buttonFrame.pack(side=tk.BOTTOM)
+    def on_closing():
+        sys.exit()
 
+    partPage.protocol("WM_DELETE_WINDOW", on_closing)
 
     partPage.mainloop()
 
